f4910e78aca5f72ec438edb7e4fdeee000386111
Kevin Iseh-Ntah <keviniseh3@gmail.com>
2026-09-19 22:57:00 -1100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,31 +4,12 @@ import { MetadataRoute } from 'next';
 const BASE_URL = 'https://conciergego.vercel.app';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Add all the active paths on your website here
   return [
     {
       url: `${BASE_URL}`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1.0,
-    },
-    {
-      url: `${BASE_URL}/services`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${BASE_URL}/about`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
-      url: `${BASE_URL}/contact`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
     },
   ];
 }
